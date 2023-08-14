a928c7227ba1fc00308517061d5adac21ca94ea3
[vibha-sheladiya] <[vibhasheladiya3936@gmail.com]>
2023-08-14 10:26:36 +0400

    sucessfuly add data in compass and insomina with cluster

--- node/src/routes/v1/user.route.js
+++ node/src/routes/v1/user.route.js
@@ -1,18 +1,27 @@
 const express=require('express');
-const {userValidation}= require('../../validations');
+// const {userValidation}= require('../../validations');
 const {userController}=require('../../controller');
-const validate=require('../../middlewares/validate');
+// const validate=require('../../middlewares/validate');
 const router=express.Router();
+
 // create user 
 router.post("/create-user",
-validate(userValidation.createUser),
-userController  .createUser
+// validate(userValidation.createUser),
+userController.createUser
 );
+// router.post('/list',(req,res)=>{
+//     res.write('ijkk');
+//     res.end();
+// })
 
 // get user list 
-router.get("/list",
-validate(userValidation.getUserList),
-userController.getUserList
-);
+// router.post("/create-user",
+// // validate(userValidation.getUserList),
+// userController.getUserList
+// );
 
+// get user list simple api
+// router.post('/create-user',(req,res)=>{
+//     res.send("connected succesfuly");
+// });
 module.exports=router;
